4896c1caa7a50acba64169a7ac5e1464971e8b25
Christophe Courtaut <christophe.courtaut@gmail.com>
2015-06-02 15:09:42 +0200

    Debug datasources

--- libraries/provider_grafana_datasource.rb
+++ libraries/provider_grafana_datasource.rb
@@ -50,6 +50,7 @@ class Chef
           password: new_resource.password
         }
         datasources = get_data_source_list(grafana_options)
+        Chef::Log.warn(datasources)
 
         exists = false
         datasources.each do |src|
